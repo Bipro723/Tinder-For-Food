35f7f516e2b5c8d87ddbb447ec1b5818e0f6d6f3
ElvDu <manidonteven@gmail.com>
2019-04-22 02:57:06 -0400

    very-basic-settingslayout
    -laid out the basic elements for settingslayout
    -specific food preferences need to be filled out; use the categories provided by Yelp API

--- app/src/main/java/com/group6/tinderforfood/SettingsActivity.java
+++ app/src/main/java/com/group6/tinderforfood/SettingsActivity.java
@@ -1,14 +1,12 @@
 package com.group6.tinderforfood;
 
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
-public class ProfileActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profilelayout);
+        setContentView(R.layout.settingslayout);
     }
 }
