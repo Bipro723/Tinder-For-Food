d58e035bf432e9fd36117d828f571106892f7398
ElvDu <manidonteven@gmail.com>
2019-04-23 23:30:06 -0400

    back-navigation
    -added back button to Profile and Settings

--- app/src/main/java/com/group6/tinderforfood/SettingsActivity.java
+++ app/src/main/java/com/group6/tinderforfood/SettingsActivity.java
@@ -8,5 +8,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settingslayout);
+
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+
+    }
+    @Override
+    public boolean onSupportNavigateUp(){ //this changes the back button function
+        finish(); //this finishes current activity, making the app go back to the previous activity
+        return true;
     }
 }
